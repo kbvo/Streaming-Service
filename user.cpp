e65ac950907196f4b04b3202bd0c0100dbf2245d
Kevin Vo <149135851+kbvo@users.noreply.github.com>
2024-11-21 20:25:59 -0800

    Update user.cpp

--- user.cpp
+++ user.cpp
@@ -3,13 +3,11 @@
 #include <iostream>
 using namespace std;
 
-// Complete - Do not alter
 void User::addToHistory(CID_T contentID)
 {
     history.push_back(contentID);
 }
 
-// Complete - Do not alter
 bool User::haveWatched(CID_T contentID)
 {
     auto my_iterator = std::find(history.begin(), history.end(), contentID);
